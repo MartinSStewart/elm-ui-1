ac5e7f3c5a1d457cd815acd96c70f88e4349080f
Matthew Griffith <mdg.griffith@gmail.com>
2024-03-30 14:39:59 -0400

    input slide render test

---
+++ tests-rendering/visual/Input/Slide.elm
@@ -0,0 +1,118 @@
+module Input.Slide exposing (main)
+
+{-| -}
+
+import Browser
+import Html exposing (Html)
+import Html.Attributes as Attr
+import Html.Events
+import Theme
+import Ui
+import Ui.Font
+import Ui.Input
+import Ui.Prose
+
+
+main =
+    Browser.document
+        { init = \() -> ( { value = 0 }, Cmd.none )
+        , update = update
+        , view =
+            \model ->
+                { title = "Slider"
+                , body = [ view model ]
+                }
+        , subscriptions = \_ -> Sub.none
+        }
+
+
+update msg model =
+    case msg of
+        SliderUpdated value ->
+            ( { model | value = value }, Cmd.none )
+
+
+type Msg
+    = SliderUpdated Float
+
+
+view model =
+    Ui.layout
+        [ Ui.Font.family [ Ui.Font.typeface "Garamond EB" ]
+        ]
+        (Ui.column
+            [ Ui.width (Ui.px 600)
+
+            -- , Ui.centerX
+            -- , Ui.height Ui.fill
+            , Ui.paddingXY 0 100
+            , Ui.spacing 100
+            ]
+            [ Theme.h1 "Slider"
+            , Ui.el
+                []
+                (Ui.html
+                    (Html.input
+                        [ Attr.type_ "range"
+                        , Attr.min "-500"
+                        , Attr.max "500"
+                        , Attr.class "s e"
+                        , Attr.value (String.fromFloat model.value)
+
+                        -- , Attr.style "width" "100%"
+                        , Attr.style "height" "300px"
+                        , Attr.attribute "orient" "vertical"
+                        , Attr.style "appearance" "slider-vertical"
+                        , Html.Events.onInput
+                            (\str ->
+                                case String.toFloat str of
+                                    Nothing ->
+                                        -- This should never happen because the browser
+                                        -- should always provide a Float.
+                                        SliderUpdated 0
+
+                                    Just val ->
+                                        SliderUpdated val
+                            )
+                        ]
+                        []
+                    )
+                )
+            , Ui.Input.sliderHorizontal
+                [ Ui.borderColor (Ui.rgba 0 0 0 1)
+                , Ui.border 1
+                , Ui.padding 2
+                , Ui.rounded 10
+
+                -- , Theme.rulerTop 100
+                -- , Ui.width (Ui.px 300)
+                ]
+                { onChange = SliderUpdated
+                , min = -500
+                , max = 500
+                , value = model.value
+                , thumb = Nothing
+                , step = Nothing
+                , label =
+                    Ui.Input.labelHidden "Type something here..."
+                }
+            , Ui.text (String.fromFloat model.value)
+            , Ui.Input.sliderVertical
+                [ Ui.borderColor (Ui.rgba 0 0 0 1)
+                , Ui.border 1
+                , Ui.padding 2
+                , Ui.rounded 10
+                , Ui.height (Ui.px 300)
+                , Ui.centerX
+                ]
+                { onChange = SliderUpdated
+                , min = -500
+                , max = 500
+                , value = model.value
+                , thumb = Nothing
+                , step = Nothing
+                , label =
+                    Ui.Input.labelHidden "Type something here..."
+                }
+            ]
+        )
