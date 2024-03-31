f199ff4f996259a4fe755f693957a2c271cfa1e7
Matthew Griffith <mdg.griffith@gmail.com>
2024-03-31 13:11:55 -0400

    More table tests

--- tests-rendering/visual/Tables.elm
+++ tests-rendering/visual/Tables.elm
@@ -25,6 +25,10 @@ myTable =
                     Ui.Table.cell []
                         (Ui.text row.occupation)
             }
+            |> Ui.Table.withVisibility
+                (\{ visibleOccupation } ->
+                    visibleOccupation
+                )
         , Ui.Table.column
             { header =
                 Ui.Table.cell
@@ -47,7 +51,25 @@ myTable =
                 , min = Nothing
                 , max = Nothing
                 }
+            |> Ui.Table.withSummary
+                (\_ rows ->
+                    let
+                        total =
+                            List.sum (List.map .salary rows)
+                    in
+                    ("Total: " ++ String.fromInt total)
+                        |> Ui.text
+                        |> Ui.Table.cell [ Ui.Font.alignRight ]
+                )
         ]
+        |> Ui.Table.withSort
+            (\{ sortedBySalary } rows ->
+                if sortedBySalary then
+                    List.sortBy (\row -> row.salary) rows
+
+                else
+                    rows
+            )
 
 
 data =
@@ -68,6 +90,30 @@ data =
             ]
 
 
+type alias State =
+    { sortedBySalary : Bool
+    , visibleOccupation : Bool
+    }
+
+
+state =
+    { visibleOccupation = True
+    , sortedBySalary = False
+    }
+
+
+hidden =
+    { visibleOccupation = False
+    , sortedBySalary = False
+    }
+
+
+sorted =
+    { visibleOccupation = True
+    , sortedBySalary = True
+    }
+
+
 main : Html msg
 main =
     Ui.layout []
@@ -78,7 +124,36 @@ main =
             , Ui.spacing 100
             ]
             [ Theme.h1 "Row"
-            , Ui.Table.view [] myTable data
-            , Ui.el [ Ui.Font.alignRight ] (Ui.text "testestests")
+            , Ui.Table.viewWithState [] myTable state data
+            , Theme.h1 "Scrollable!"
+            , Ui.Table.viewWithState
+                [ Ui.inFront
+                    (Ui.el
+                        [ Ui.width (Ui.px 200)
+                        , Ui.height (Ui.px 200)
+                        , Ui.alignRight
+                        , Ui.background (Ui.rgb 255 0 0)
+                        ]
+                        Ui.none
+                    )
+                , Ui.height (Ui.px 400)
+                ]
+                (myTable
+                    |> Ui.Table.withScrollable { stickFirstColumn = False }
+                )
+                state
+                data
+            , Theme.h1 "Hidden columns!"
+            , Ui.Table.viewWithState
+                []
+                myTable
+                hidden
+                data
+            , Theme.h1 "Sorted by salary!"
+            , Ui.Table.viewWithState
+                []
+                myTable
+                sorted
+                data
             ]
         )
